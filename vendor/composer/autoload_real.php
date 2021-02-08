5e418ca5f4d25d87720c4df420ee651c7a415cd9
genresos <akunryanpambudi@gmail.com>
2021-02-08 23:53:11 +0700

    fixed

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInit9e1e51ece255f8376454a3d549b8b169
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit9e1e51ece255f8376454a3d549b8b169', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit9e1e51ece255f8376454a3d549b8b169', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit9e1e51ece255f8376454a3d549b8b169::getInitializer($loader));
         } else {
